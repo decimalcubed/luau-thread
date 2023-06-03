06c8465aa65fb2b3709bfbfa0e68cc2c04f8090e
decimalcubed <77302966+decimalcubed@users.noreply.github.com>
2023-06-03 16:54:39 +0100

    Create Processor_Server.server.lua

---
+++ src/Processor_Server.server.lua
@@ -0,0 +1,20 @@
+---!strict
+
+local threadFinishedSignal = script.Parent.Parent:FindFirstChild("ThreadFinished") :: BindableEvent
+--This could be done by having it be a value in the main thread module but it would use more memory and be slightly more annoying to type so idc
+
+local selfActor = script:GetActor()
+selfActor:BindToMessage("RunThread", function(thread_id: number, execute_module: ModuleScript, ...)
+
+	local execute = require(execute_module)
+
+	--Execute execute_module
+	task.desynchronize()
+	execute(...)
+	task.synchronize()
+
+	--Resume all threads waiting on this
+	threadFinishedSignal:Fire(thread_id, selfActor)
+end)
+
+return nil
