2ca538e3065c07cc64dc0a5440f20b3bfababe1a
decimalcubed <77302966+decimalcubed@users.noreply.github.com>
2023-12-09 01:02:38 +0000

    Optimize init.lua
    Remove debug profiles
    Switch to coroutine library for resuming yielding threads
    Add --!native and --!optimize 2 tags for studio performance and studio testing

--- src/init.lua
+++ src/init.lua
@@ -1,22 +1,14 @@
 --!strict
+--!native
+--!optimize 2
 
 local thread = {}
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local runContextIsClient = game:GetService("RunService"):IsClient()
 local processorToUse = if runContextIsClient then script.Processor_Client else script.Processor_Server
-local processorParent = if runContextIsClient then game:GetService("Players").LocalPlayer.PlayerScripts else game:GetService("ServerScriptService")
+local processorParent = script
 
--- Get thread finished signal (Or create it if it already exists, which could be the case if it was placed there in the editor or if the server made it)
-local threadFinishedSignal = ReplicatedStorage:FindFirstChild("ThreadFinished") :: BindableEvent?
-if not threadFinishedSignal then
-	
-	threadFinishedSignal = Instance.new("BindableEvent")
-	assert(threadFinishedSignal) --Typechecker wack
-	
-	threadFinishedSignal.Name = "ThreadFinished"
-	threadFinishedSignal.Parent = ReplicatedStorage
-end
-assert(threadFinishedSignal)
+-- Get thread finished signal
+local threadFinishedSignal = script.ThreadFinished
 
 -- Thread tracking
 local highestThreadId = 0
@@ -25,8 +17,6 @@ local activeThreads: {{thread}} = {}
 -- Actor tracking
 local actorCache: {Actor} = {}
 local function BuildActor(): Actor
-	
-	debug.profilebegin("thread_BuildActor")
 	
 	--Build processor and actor
 	local actor = Instance.new("Actor")
@@ -38,8 +28,6 @@ local function BuildActor(): Actor
 
 	--Enable processor
 	processor.Enabled = true
-	
-	debug.profileend()
 
 	return actor
 end
@@ -49,8 +37,6 @@ end
 ]]
 
 function thread.spawn(execute_module: ModuleScript, ...): number
-	
-	debug.profilebegin("thread_spawn")
 
 	highestThreadId += 1
 
@@ -59,10 +45,7 @@ function thread.spawn(execute_module: ModuleScript, ...): number
 
 	--Mark the current ID as active and start the thread
 	activeThreads[highestThreadId] = {};
-
 	actor:SendMessage("RunThread", highestThreadId, execute_module, ...)
-	
-	debug.profileend()
 	
 	return highestThreadId
 end
@@ -102,12 +85,10 @@ end
 -- Connect to the thread finished signal to respawn join coroutines
 threadFinishedSignal.Event:Connect(function(id: number, actor: Actor)
 	
-	debug.profilebegin("thread_internal_ResumeYieldedThreads")
-	
 	local active_thread = activeThreads[id]
 	for _, v in active_thread do
-
-		task.spawn(v)
+		
+		coroutine.resume(v)
 	end
 
 	--Disconnect and clean up
@@ -115,7 +96,6 @@ threadFinishedSignal.Event:Connect(function(id: number, actor: Actor)
 
 	--Add the actor back to the actor cache
 	table.insert(actorCache, actor)
-	debug.profileend()
 end)
 
 return table.freeze(thread)
