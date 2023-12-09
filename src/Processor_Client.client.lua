f21084a060754cfada3136732305a3be81c61c34
decimalcubed <77302966+decimalcubed@users.noreply.github.com>
2023-12-09 01:29:53 +0000

    Update Processor_Client.client.lua

--- src/Processor_Client.client.lua
+++ src/Processor_Client.client.lua
@@ -1,22 +1,20 @@
----!strict
+--!strict
+--!optimize 2
 
 local threadFinishedSignal = game:GetService("ReplicatedStorage"):WaitForChild("ThreadFinished") :: BindableEvent
+local threadCommunicationSignal = script.Parent:FindFirstChild("ThreadCommunicationSignal") :: BindableEvent
 --This could be done by having it be a value in the main thread module but it would use more memory and be slightly more annoying to type so idc
 
 local selfActor = script:GetActor()
-selfActor:BindToMessage("RunThread", function(thread_id: number, execute_module: ModuleScript, ...)
-	
-	local execute = require(execute_module)
+threadCommunicationSignal.Event:Connect(function(thread_id: number, execute_module: ModuleScript, ...)
+
+	local execute = (require :: any)(execute_module)
 
 	--Execute execute_module
 	task.desynchronize()
-	debug.profilebegin("thread_client_processor_execute")
 	execute(...)
-	debug.profileend()
 	task.synchronize()
 
 	--Resume all threads waiting on this
-	debug.profilebegin("thread_client_processor_finish")
 	threadFinishedSignal:Fire(thread_id, selfActor)
-	debug.profileend()
 end)
