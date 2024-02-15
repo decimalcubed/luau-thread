234f6a989d9b2ac69a179a3269caf860fd4ebfc4
decimalcubed <77302966+decimalcubed@users.noreply.github.com>
2024-02-15 01:21:12 +0000

    undo previous comit it was shit
    found a better way to do it

--- src/init.lua
+++ src/init.lua
@@ -12,19 +12,18 @@ local threadFinishedSignal = script.ThreadFinished
 
 -- Thread tracking
 local highestThreadId = 0
-local yieldingThreads: {{thread}} = {}
-local signalProcessors: {[BindableEvent]: Script} = {}
+local activeThreads: {{thread}} = {}
 
 -- Actor tracking
-local signalCache: {[Script]: {BindableEvent}} = {}
-local function BuildVM(processor: Script): BindableEvent
+local signalCache: {BindableEvent} = {}
+local function BuildVM(): BindableEvent
 	
 	-- Build processor and actor
 	local actor = Instance.new("Actor")
 	actor.Name = "ThreadActor"
 	actor.Parent = processorParent
 	
-	local processor = processor:Clone()
+	local processor = processorToUse:Clone()
 	processor.Parent = actor
 	
 	-- Create event
@@ -34,8 +33,7 @@ local function BuildVM(processor: Script): BindableEvent
 
 	--Enable processor
 	processor.Enabled = true
-	
-	signalProcessors[communication_signal] = processor
+
 	return communication_signal :: any
 end
 
@@ -45,26 +43,15 @@ end
 
 function thread.spawn(execute_module: ModuleScript, ...): number
 
-	return thread.spawn_custom(processorToUse, execute_module, ...)
-end
-
-function thread.spawn_custom(processor: Script, execute_module: ModuleScript, ...): number
-
 	highestThreadId += 1
-	
-	-- Ensure there is a signal cache for the given processor
-	if not signalCache[processor] then
-		
-		signalCache[processor] = {}
-	end
 
 	-- Get the last available signal or build new one
-	local signal = table.remove(signalCache[processor]) or BuildVM(processor)
+	local signal = table.remove(signalCache) or BuildVM()
 
 	-- Mark the current ID as active and start the thread
-	yieldingThreads[highestThreadId] = {}; --TODO this can be made a buffer and yielding threads can be tracked using an id hashmap
+	activeThreads[highestThreadId] = {}; --TODO this can be made a buffer and yielding threads can be tracked using an id hashmap
 	signal:Fire(highestThreadId, execute_module, ...)
-
+	
 	return highestThreadId
 end
 
@@ -75,7 +62,7 @@ function thread.join(thread_id: number | { number })
 		for _, thread_id in thread_id do
 
 			-- Continue if the given thread has already finished
-			local active_thread = yieldingThreads[thread_id]
+			local active_thread = activeThreads[thread_id]
 			if not active_thread then
 
 				continue
@@ -88,7 +75,7 @@ function thread.join(thread_id: number | { number })
 	else
 
 		-- Return instantly if the given thread has already finished
-		local active_thread = yieldingThreads[thread_id]
+		local active_thread = activeThreads[thread_id]
 		if not active_thread then
 
 			return
@@ -103,17 +90,17 @@ end
 -- Connect to the thread finished signal to respawn join coroutines
 threadFinishedSignal.Event:Connect(function(id: number, signal: BindableEvent)
 	
-	local yielding_threads = yieldingThreads[id]
-	for _, v in yielding_threads do
+	local active_thread = activeThreads[id]
+	for _, v in active_thread do
 		
 		coroutine.resume(v)
 	end
 
 	-- Disconnect and clean up
-	yieldingThreads[id] = nil
+	activeThreads[id] = nil
 
 	-- Add the signal back to the signal cache
-	table.insert(signalCache[signalProcessors[signal]], signal)
+	table.insert(signalCache, signal)
 end)
 
 return table.freeze(thread)
